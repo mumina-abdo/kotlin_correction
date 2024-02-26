aced7f531384dddf92d19864ebb87e9ce8a05265
mumina-abdo <muminaboruabdo@gmail.com>
2024-02-26 07:31:35 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main(){
+  name("Mumina")
+    var z =division(8, 2.0)
+    println(z)
+add(40, 60,80,120)
+println("i love watching football")
+}
+
+
+
+
+
+
+fun name(name:String){
+    var greetings="Hello ${name}"
+    println(greetings)
+}
+
+fun division(num1:Int,num2:Double):Double{
+    var remender=num1%num2
+    return  remender
+
+
+}
+fun add(x:Int,y:Int,z:Int,p:Int):Int {
+    var sum = (x+y+z+p)
+    return sum
+
+
+
+
+}
+fun fact (interesting:String){
+    var interesting="i love watching football"
+    println(interesting)
+}
